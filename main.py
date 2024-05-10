de26f7d2524919d0d163c9d97f3fdc4759a71674
Aitoshas <168238284+Aitoshas@users.noreply.github.com>
2024-05-10 22:56:38 +0600

    main-base-code

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+import sqlite3
+import random
+
+pygame.init()
+
+WIDTH,HEIGHT = 800,600
+pygame.diplay.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Quiz")
+
+
+
+pygame.quit()
